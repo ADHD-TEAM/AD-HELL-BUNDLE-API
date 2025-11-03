b603b4afba410605a72f1e935f44a0670e1bc0f9
kim100000000 <kst980510@gmail.com>
2025-11-03 19:22:46 +0900

    fix : ErrorCode수정사항

--- ad-hell/src/main/java/com/adhd/ad_hell/exception/ErrorCode.java
+++ ad-hell/src/main/java/com/adhd/ad_hell/exception/ErrorCode.java
@@ -24,9 +24,13 @@ public enum ErrorCode {
   REWARD_STOCK_INVALID_STATUS("40011", "이미 사용되었거나 만료된 경품입니다.", HttpStatus.BAD_REQUEST),
 
   // 게시판 관련 에러코드
-  BOARD_NOT_FOUND("70001", "게시판을 찾지 못했습니다.", HttpStatus.NOT_FOUND),
+  BOARD_NOT_FOUND("60001", "게시판을 찾지 못했습니다.", HttpStatus.NOT_FOUND),
+    // 게시글 관련 에러코드
+    COMMENT_NOT_FOUND("70001", "게시글을 찾지 못했습니다.", HttpStatus.NOT_FOUND),
+    // 공지사항 관련 에러코드
+    ANNOUNCEMENT_NOT_FOUND("80001", "공지사항을 찾지 못했습니다.", HttpStatus.NOT_FOUND),
     // 문의 관련 에러코드
-    INQUIRY_NOT_FOUND("80001", "문의를 찾지 못했습니다.", HttpStatus.NOT_FOUND),
+    INQUIRY_NOT_FOUND("90001", "문의를 찾지 못했습니다.", HttpStatus.NOT_FOUND),
 
 
   //파일 관련 에러코드
