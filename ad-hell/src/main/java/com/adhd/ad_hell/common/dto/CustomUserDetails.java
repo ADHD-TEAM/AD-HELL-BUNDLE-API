47526cd4401a5f5becf216b07106b7ee96cb3725
lala19873@naver.com <lala19873@naver.com>
2025-11-03 18:16:10 +0900

    SecurityUtil추가 -로그인 정보 가져오는 로직 추가

--- ad-hell/src/main/java/com/adhd/ad_hell/common/dto/CustomUserDetails.java
+++ ad-hell/src/main/java/com/adhd/ad_hell/common/dto/CustomUserDetails.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
 
+    private final Long userId;
     private final String loginId;
     private final String password;
     private final Role role;
