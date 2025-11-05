532c0e7cb41f56c084bcb8e11ccd205dbad333db
lala19873@naver.com <lala19873@naver.com>
2025-11-05 16:37:12 +0900

    회원가입, 관리자- 회원 정보 수정 리팩토링

--- ad-hell/src/main/java/com/adhd/ad_hell/domain/user/command/dto/response/UserDetailResponse.java
+++ ad-hell/src/main/java/com/adhd/ad_hell/domain/user/command/dto/response/UserDetailResponse.java
@@ -2,6 +2,7 @@ package com.adhd.ad_hell.domain.user.command.dto.response;
 
 import com.adhd.ad_hell.domain.user.command.entity.User;
 import com.adhd.ad_hell.domain.user.command.entity.UserStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,7 +16,11 @@ public class UserDetailResponse {
     private String nickname;
     private String email;
     private UserStatus status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime deactivatedAt;
     private Integer amount;
 
